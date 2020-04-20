415a736e4b57401971cff12d505ad5e611d6a313
Raditya Pratama <radityaapratamaa@gmail.com>
2020-04-21 00:10:05 +0700

    implement Flate Class on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/config/routes.dart';
-import 'package:shop_app/config/theme.dart';
+import 'package:shop_app/_core_transform_in/flate.dart';
 import 'package:shop_app/pages/dashboard.dart';
 
 void main() => runApp(MyApp());
@@ -9,21 +8,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GosMaster Flutter Templates',
-      theme: implementAndoidTheme(),
-      routes: getRoutes(
-        context,
-        defaultRouteWidget: DashboardPage(),
-      ),
-      onUnknownRoute: (settings) {
-        print(settings.name);
-        return;
-      },
-      onGenerateRoute: (settings) {
-        print(settings.name);
-        return;
-      },
+    return Flate(
+      context,
+      'Flate Page Template',
+    ).buildAndroidPage(
+      defaultRouteWidget: DashboardPage(),
     );
   }
 }
